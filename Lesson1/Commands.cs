1bae8ed12d30522b1f57d5916a1b1d2e38638d21
smxshimamura <tatsunori.shimamura@systemmetrix.jp>
2024-11-06 07:42:25 +0900

    Lesson1 has been completed and committed.

---
+++ Lesson1/Commands.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Commands.cs" company="Tutorial">
+//   Tutorial codes.
+// </copyright>
+// <summary>
+//   Defines the Commands type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Autodesk.AutoCAD.Runtime;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+
+[assembly: CommandClass(typeof(Lesson1.Commands))]
+[assembly: ExtensionApplication(typeof(Lesson1.PluginExtension))]
+
+namespace Lesson1
+{
+    /// <summary>
+    ///  Provides any commands.
+    /// </summary>
+    public class Commands
+    {
+        /// <summary>
+        /// Execute Hello command.
+        /// </summary>
+        [CommandMethod("Tutorials", "Hello", "HelloId", CommandFlags.Modal)]
+        public void Hello()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc != null)
+            {
+                doc.Editor.WriteMessage("Hello, World!" + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ Lesson1/PluginExtension.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PluginExtension.cs" company="Tutorial">
+//   Tutorial codes.
+// </copyright>
+// <summary>
+//   Defines the Commands type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Lesson1
+{
+    /// <summary>
+    ///  The plugin for extension.
+    /// </summary>
+    /// <seealso cref="Autodesk.AutoCAD.Runtime.IExtensionApplication" />
+    public class PluginExtension : Autodesk.AutoCAD.Runtime.IExtensionApplication
+    {
+        /// <summary>
+        /// Initializes this instance.
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Terminates this instance.
+        /// </summary>
+        public void Terminate()
+        {
+        }
+    }
+}
