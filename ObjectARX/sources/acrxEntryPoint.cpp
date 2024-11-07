eb0b29411970e371cc10f4ca0337e0556cf25a16
smxshimamura <tatsunori.shimamura@systemmetrix.jp>
2024-11-07 15:30:56 +0900

    Initial commit for ObjectARX.

---
+++ ObjectARX/sources/acrxEntryPoint.cpp
@@ -0,0 +1,86 @@
+// (C) Copyright 2002-2012 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+//-----------------------------------------------------------------------------
+//----- acrxEntryPoint.cpp
+//-----------------------------------------------------------------------------
+#include "StdAfx.h"
+#include "resource.h"
+
+//-----------------------------------------------------------------------------
+#define szRDS _RXST("ADSK")
+
+//-----------------------------------------------------------------------------
+//----- ObjectARX EntryPoint
+class ARXTrainingApp : public AcRxArxApp {
+
+public:
+	ARXTrainingApp() : AcRxArxApp () {}
+
+	virtual AcRx::AppRetCode On_kInitAppMsg (void *pkt) {
+		// TODO: Load dependencies here
+
+		// You *must* call On_kInitAppMsg here
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kInitAppMsg (pkt) ;
+		
+		// TODO: Add your initialization code here
+
+		return (retCode) ;
+	}
+
+	virtual AcRx::AppRetCode On_kUnloadAppMsg (void *pkt) {
+		// TODO: Add your code here
+
+		// You *must* call On_kUnloadAppMsg here
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kUnloadAppMsg (pkt) ;
+
+		// TODO: Unload dependencies here
+
+		return (retCode) ;
+	}
+
+	virtual void RegisterServerComponents () {
+	}
+	
+	// The ACED_ARXCOMMAND_ENTRY_AUTO macro can be applied to any static member 
+	// function of the ARXTrainingApp class.
+	// The function should take no arguments and return nothing.
+	//
+	// NOTE: ACED_ARXCOMMAND_ENTRY_AUTO has overloads where you can provide resourceid and
+	// have arguments to define context and command mechanism.
+	
+	// ACED_ARXCOMMAND_ENTRY_AUTO(classname, group, globCmd, locCmd, cmdFlags, UIContext)
+	// ACED_ARXCOMMAND_ENTRYBYID_AUTO(classname, group, globCmd, locCmdId, cmdFlags, UIContext)
+	// only differs that it creates a localized name using a string in the resource file
+	//   locCmdId - resource ID for localized command
+
+	// Modal Command with localized name
+	// ACED_ARXCOMMAND_ENTRY_AUTO(ARXTrainingApp, ADSKMyGroup, MyCommand, MyCommandLocal, ACRX_CMD_MODAL)
+	static void ADSKMyGroupMyCommand () {
+		// Put your command code here
+		acutPrintf(_T("\r\nHello World!\r\n"));
+	}
+
+} ;
+
+//-----------------------------------------------------------------------------
+IMPLEMENT_ARX_ENTRYPOINT(ARXTrainingApp)
+ACED_ARXCOMMAND_ENTRY_AUTO(ARXTrainingApp, ADSKMyGroup, MyCommand, MyCommand, ACRX_CMD_MODAL, NULL)
